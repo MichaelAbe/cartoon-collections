a8daec18823145375db8fd2f7e1489246378f1eb
MichaelAbe <michael.k.abe@gmail.com>
2020-11-13 00:02:15 -0500

    commit tree 7fe211c1238314020f519fe12ae5207c266d1c6f

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(squad)
+   squad.each_with_index do |squad, place|
+    puts "#{place + 1}. #{squad}"
+    end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(elements)
+    with_your_powers_combined = []
+    elements.each {|call| with_your_powers_combined << call.capitalize + "!"}
+    with_your_powers_combined
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(call)
+    call.any? {|i| i.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(element)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  blah = []
+  cheese_types.map do |i|
+    element.include?(i) ? blah << i : element.shift 
+  end
+  blah[0]
 end
